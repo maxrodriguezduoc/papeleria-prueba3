c8f1418f9bf350d605a8e670cbc736e517f8b7c6
Ignacio Acevedo <ig.acevedoe@duocuc.cl>
2026-06-25 18:22:21 -0400

    Se añaden assembler y controller V2 de Comuna

---
+++ Local/src/main/java/Ubicacion/Local/assembler/ComunaModelAssembler.java
@@ -0,0 +1,35 @@
+package Ubicacion.Local.assembler;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import Ubicacion.Local.controller.v2.ComunaControllerV2;
+import Ubicacion.Local.dto.ComunaDTO;
+
+@Component
+public class ComunaModelAssembler implements RepresentationModelAssembler<ComunaDTO, EntityModel<ComunaDTO>>{
+
+    @Override
+    public EntityModel<ComunaDTO> toModel(ComunaDTO comuna){
+        return EntityModel.of(comuna,
+                linkTo(methodOn(ComunaControllerV2.class)
+                .buscar(comuna.getIdComuna()))
+                .withSelfRel(),
+
+                linkTo(methodOn(ComunaControllerV2.class)
+                .listar())
+                .withRel("comunas"),
+
+                linkTo(methodOn(ComunaControllerV2.class)
+                .actualizar(comuna.getIdComuna(), null))
+                .withRel("actualizar"),
+
+                linkTo(methodOn(ComunaControllerV2.class)
+                .eliminar(comuna.getIdComuna()))
+                .withRel("eliminar")
+        );
+    } 
+}
