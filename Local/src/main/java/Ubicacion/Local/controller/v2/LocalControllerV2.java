6b9f0c9421fc62bbbef871b4bcf04f6d0ba58b2e
Ignacio Acevedo <ig.acevedoe@duocuc.cl>
2026-06-27 21:53:43 -0400

    Ajustes controllers V2

--- Local/src/main/java/Ubicacion/Local/controller/v2/LocalControllerV2.java
+++ Local/src/main/java/Ubicacion/Local/controller/v2/LocalControllerV2.java
@@ -1,16 +1,20 @@
 package Ubicacion.Local.controller.v2;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,47 +38,56 @@ public class LocalControllerV2 {
     @Autowired
     private LocalModelAssembler assembler;
 
-    @GetMapping
-    public CollectionModel<EntityModel<LocalDTO>> listar(){
+    @PostMapping(produces = MediaTypes.HAL_JSON_VALUE)
+    public ResponseEntity<EntityModel<LocalDTO>> crear(@Valid @RequestBody Local local) {
+        log.info("API REST V2 - PETICIÓN PARA POSTEAR LOCALES: '{}'", local.getNombreLocal());
+        
+        LocalDTO nuevoLocal = localService.guardarLocal(local);
+        
+        return ResponseEntity
+                .created(linkTo(methodOn(LocalControllerV2.class).buscar(nuevoLocal.getIdLocal())).toUri())
+                .body(assembler.toModel(nuevoLocal));
+    }
 
-        log.info("API REST V2 - PETICIÓN PARA LISTAR LOCALES ACTIVOS");
-
-        List<EntityModel<LocalDTO>> locales = localService.obtenerTodos()
-                .stream()
+    @GetMapping(produces = MediaTypes.HAL_JSON_VALUE)
+    public ResponseEntity<CollectionModel<EntityModel<LocalDTO>>> listar() {
+        log.info("API REST V2 - PETICIÓN PARA LISTAR LOCALES");
+        
+        List<EntityModel<LocalDTO>> locales = localService.obtenerTodos().stream()
                 .map(assembler::toModel)
-                .toList();
-
-        return CollectionModel.of(locales,
-                linkTo(methodOn(LocalControllerV2.class).listar()).withSelfRel());
+                .collect(Collectors.toList());
+        
+        if (locales.isEmpty()) {
+            log.info("LA CONSULTA DE LOCALES NO RETORNO REGISTROS ACTIVOS");
+            return ResponseEntity.noContent().build();
+        }
+        
+        return ResponseEntity.ok(CollectionModel.of(
+            locales, linkTo(methodOn(ComunaControllerV2.class).listar()).withSelfRel()
+        ));
     }
 
-    @GetMapping("/{id}")
-    public EntityModel<LocalDTO> buscar(@PathVariable Integer id){
-
-        log.info("API REST V2 - PETICIÓN PARA BUSCAR LOCAL POR ID: {}", id);
-
+    @GetMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    public ResponseEntity<EntityModel<LocalDTO>> buscar(@PathVariable Integer id) {
+        log.info("API REST V2 - PETICIÓN PARA BUSCAR LOCAL CON ID: {}", id);
+        
         LocalDTO local = localService.buscarPorId(id);
-
-        return assembler.toModel(local);
+        return ResponseEntity.ok(assembler.toModel(local ));
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<EntityModel<LocalDTO>> actualizar(@PathVariable Integer id, @Valid @RequestBody Local local){
-
+    @PutMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    public ResponseEntity<EntityModel<LocalDTO>> actualizar(@PathVariable Integer id, @Valid @RequestBody Local local) {
         log.info("API REST V2 - PETICIÓN PARA ACTUALIZAR LOCAL CON ID: {}", id);
-
-        LocalDTO actualizado = localService.actualizarLocal(id, local);
-
-        return ResponseEntity.ok(assembler.toModel(actualizado));
+        
+        LocalDTO actualizada = localService.actualizarLocal(id, local);
+        return ResponseEntity.ok(assembler.toModel(actualizada));
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<String> eliminar(@PathVariable Integer id){
-
-        log.warn("API REST V2 - PETICIÓN PARA DESACIVAR LOCAL CON ID: {}", id);
-
+    @DeleteMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    public ResponseEntity<Void> eliminar(@PathVariable Integer id) {
+        log.warn("API REST V2 - PETICIÓN PARA ELIMINAR LOCAL CON ID: {}", id);
+        
         localService.eliminarLocal(id);
-
-        return ResponseEntity.ok("LOCAL CON ID " + id + " ELIMINADO CON EXITO!");
+        return ResponseEntity.noContent().build(); 
     }
 }
